8ab03d802ae02fb859eca9ad52ec719e7a281c2f
onlyjoon <nhj7334@icloud.com>
2024-05-07 23:48:44 +0900

    feat: ch6-4 - BDDMockito

--- src/test/java/com/example/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/com/example/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -6,19 +6,16 @@ import com.example.cafekiosk.spring.domain.history.mail.MailSendHistoryRepositor
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class MailServiceTest {
@@ -35,15 +32,19 @@ class MailServiceTest {
 
     @Test
     @DisplayName("메일 전송 테스트")
-    void sendMail() throws Exception {
+    void sendMail() {
         // given - 상황 만들기
 //        MailSendClient mailSendClient = mock(MailSendClient.class);
 //        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
 
 //        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
 
-        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
-                .thenReturn(true);
+//        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString())) // when이 왜 given에 있어?
+//                .thenReturn(true);
+
+        BDDMockito.given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
+
 //        when(mailSendHistoryRepository.save(any(MailSendHistory.class)))
 //                .thenReturn();
 //        doReturn(true)
